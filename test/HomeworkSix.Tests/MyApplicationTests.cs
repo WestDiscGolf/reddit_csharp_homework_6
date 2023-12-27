2c2e82ac5d9ba5c4fba2c48e92fb6d365bfe183e
Adam Storr <WestDiscGolf@users.noreply.github.com>
2023-12-27 22:09:23 +0000

    First round of tests

--- test/HomeworkSix.Tests/MyApplicationTests.cs
+++ test/HomeworkSix.Tests/MyApplicationTests.cs
@@ -1,10 +1,74 @@
+using AutoFixture.Xunit2;
+using Moq;
+
 namespace HomeworkSix.Tests;
 
 public class MyApplicationTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineAutoMoqData]
+    public void Welcome_Is_Displayed(
+        [Frozen] Mock<IOutputWriter> outputWriter,
+        [Frozen] Mock<IInputReader> inputReader,
+        string input,
+        MyApplication sut
+        )
     {
+        // Arrange
+        inputReader.SetupSequence(x => x.ReadLine())
+            .Returns(input)
+            .Returns("n");
 
+        // Act
+        sut.Run();
+
+        // Assert
+        outputWriter.Verify(x => x.WriteLine(It.Is<string>(s => s == "Welcome to my homework app 6!")), Times.Once);
+    }
+
+    [Theory]
+    [InlineAutoMoqData((string?)null)]
+    [InlineAutoMoqData("")]
+    public void Invalid_Name_Entered(
+        string? invalidValue,
+        [Frozen] Mock<IOutputWriter> outputWriter,
+        [Frozen] Mock<IInputReader> inputReader,
+        string input,
+        MyApplication sut
+    )
+    {
+        // Arrange
+        inputReader.SetupSequence(x => x.ReadLine())
+            .Returns(invalidValue)
+            .Returns(input)
+            .Returns("n");
+
+        // Act
+        sut.Run();
+
+        // Assert
+        outputWriter.Verify(x => x.Write(It.Is<string>(s => s == "Please enter a name: ")), Times.Exactly(2));
+        outputWriter.Verify(x => x.WriteLine(It.Is<string>(s => s == "You must enter a name.")), Times.Once);
+    }
+
+    [Theory]
+    [InlineAutoMoqData]
+    public void Successful_Initial_Entry_Displays_Awesome(
+        [Frozen] Mock<IOutputWriter> outputWriter,
+        [Frozen] Mock<IInputReader> inputReader,
+        string input,
+        MyApplication sut
+    )
+    {
+        // Arrange
+        inputReader.SetupSequence(x => x.ReadLine())
+            .Returns(input)
+            .Returns("n");
+
+        // Act
+        sut.Run();
+
+        // Assert
+        outputWriter.Verify(x => x.WriteLine(It.Is<string>(s => s == "AWESOME!")), Times.Once);
     }
 }
