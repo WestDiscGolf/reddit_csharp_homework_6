fb76172621bafba0eb35e1705ba19e07d27c4529
Adam Storr <WestDiscGolf@users.noreply.github.com>
2023-12-27 20:49:10 +0000

    Add IOutputWriter and implementation

--- src/HomeworkSix/Program.cs
+++ src/HomeworkSix/Program.cs
@@ -1,23 +1,40 @@
 ﻿
-
-var app = new MyApplication();
+var app = new MyApplication(new ConsoleWriter());
 app.Run();
 
 
+public interface IOutputWriter
+{
+    void Write(string? value = default);
 
+    void WriteLine(string? value = default);
+}
+
+public class ConsoleWriter : IOutputWriter
+{
+    public void Write(string? value = default) => Console.Write(value);
+    public void WriteLine(string? value = default) => Console.WriteLine(value);
+}
 
 public class MyApplication
 {
+    private readonly IOutputWriter _outputWriter;
+
+    public MyApplication(IOutputWriter outputWriter)
+    {
+        _outputWriter = outputWriter;
+    }
+
     public void Run()
     {
         bool isValid = false;
         string nameList;
         string yesNo;
 
-        Console.WriteLine("Welcome to my homework app 6!");
+        _outputWriter.WriteLine("Welcome to my homework app 6!");
         do
         {
-            Console.Write("Please enter a name: ");
+            _outputWriter.Write("Please enter a name: ");
             nameList = Console.ReadLine();
             isValid = !string.IsNullOrWhiteSpace(nameList);
             if (!isValid)
@@ -27,30 +44,30 @@ public class MyApplication
 
         } while (!isValid);
 
-        Console.WriteLine("AWESOME!");
+        _outputWriter.WriteLine("AWESOME!");
 
         do
         {
-            Console.Write("Would you like to add another name? Y/N ");
+            _outputWriter.Write("Would you like to add another name? Y/N ");
             yesNo = Console.ReadLine()?.ToUpper();
             isValid = !string.IsNullOrWhiteSpace(yesNo);
 
             if (!isValid)
             {
-                Console.WriteLine("Please select a valid answer.");
+                _outputWriter.WriteLine("Please select a valid answer.");
             }
             else if (yesNo == "Y")
             {
-                Console.Write("Please enter the next name: ");
+                _outputWriter.Write("Please enter the next name: ");
                 nameList += (",") + Console.ReadLine();
             }
             else if (yesNo == "N")
             {
-                Console.WriteLine("Lets move on to generating the names you've input!");
+                _outputWriter.WriteLine("Lets move on to generating the names you've input!");
             }
             else if (yesNo != "N" || yesNo != "Y")
             {
-                Console.WriteLine("Please use Y for YES and N for NO only! \n");
+                _outputWriter.WriteLine("Please use Y for YES and N for NO only! \n");
             }
 
         } while (yesNo == "Y" || yesNo != "N");
@@ -59,8 +76,8 @@ public class MyApplication
 
         foreach (string firstName in allNames)
         {
-            Console.WriteLine();
-            Console.WriteLine($"Hello, {firstName}!");
+            _outputWriter.WriteLine();
+            _outputWriter.WriteLine($"Hello, {firstName}!");
         }
     }
 }
